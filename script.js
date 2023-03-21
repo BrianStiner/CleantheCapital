ba3d64f686ad73acd9b670b2857b03fe980d88c1
MathMagicMachine <92168569+BrianStiner@users.noreply.github.com>
2023-03-20 22:53:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// Set transition percent
-const transitionPercent = 10;
+// Set scroll threshold
+const scrollThreshold = 300;
 
 // Get a reference to #logo
-const logo = document.getElementById('logo');
+const logoElement = document.getElementById('logo');
 
 // Listen for scroll events on window
 window.addEventListener('scroll', () => {
-    // Calculate percent scrolled
-    const percentScrolled = window.scrollY / (document.body.scrollHeight - window.innerHeight) * 100;
-
     // Check if #logo has .big class
-    if (logo.classList.contains('big')) {
-        // If percent scrolled > transitionPercent, remove .big class from #logo
-        if (percentScrolled > transitionPercent) {
-            logo.classList.remove('big');
+    if (logoElement.classList.contains('big')) {
+        // If window.scrollY > scrollThreshold, remove .big class from #logo
+        if (window.scrollY > scrollThreshold) {
+            logoElement.classList.remove('big');
         }
     } else {
-        // If percent scrolled < transitionPercent, add .big class to #logo
-        if (percentScrolled < transitionPercent) {
-            logo.classList.add('big');
+        // If window.scrollY < scrollThreshold, add .big class to #logo
+        if (window.scrollY < scrollThreshold) {
+            logoElement.classList.add('big');
         }
     }
 });
