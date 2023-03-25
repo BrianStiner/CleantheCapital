ee2da3005eb9cff40124726419eb826913d6fb9d
Brian Stiner <BrianStinerProfessional@gmail.com>
2023-03-25 15:58:20 -0700

    changes to resizing

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const scrollThreshold = 500;
 const logoElement = document.getElementById('logo');
 const videoBoxElement = document.getElementById('video-box');
 
+if (window.innerWidth < 768) {
+    logoElement.classList.remove('big');
+}
+
 // Listen for scroll events on window
 window.addEventListener('scroll', () => {
     // Check if #logo has .big class
@@ -12,19 +16,15 @@ window.addEventListener('scroll', () => {
         // If window.scrollY > scrollThreshold, remove .big class from #logo
         if (window.scrollY > scrollThreshold) {
             logoElement.classList.remove('big');
-            logoElement.classList.remove('set');
         }
     } else {
         // If window.scrollY < scrollThreshold, add .big class to #logo
         if (window.scrollY < scrollThreshold) {
             logoElement.classList.add('big');
-            if (window.innerWidth > 1200) {
-                logoElement.classList.add('set');
-            } else if (window.innerWidth <= 1200) {
-                logoElement.classList.remove('set');
-                calcleft = (window.innerWidth - 1200) / 2;
-                videoBoxElement.style.margin_left = '-' + calcleft + 'px';
-            }
+        }
+        if (window.innerWidth <= 1200) {
+            calcleft = (window.innerWidth - 1200) / 2;
+            videoBoxElement.style.margin_left = '-' + calcleft + 'px';
         }
     }
 });
