6ef9cdde390a2316126b8852d76cf6c1cfcd3bbb
Brian Stiner <BrianStinerProfessional@gmail.com>
2023-03-26 15:14:13 -0700

    gsap animations

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ window.addEventListener('load', function() {
     calcleft = (window.innerWidth - 1268) / 2 + 20;
     r.style.setProperty('--left', calcleft + 'px');
 
-
-    console.log(logoElement.classList.contains('big'));
     // Listen for scroll events on window, if far enough down shrink logo, and reverse.
     window.addEventListener('scroll', () => {
         if (window.innerWidth < 1268) { return; } // Don't do anything if the window is too small.
@@ -34,29 +32,33 @@ window.addEventListener('load', function() {
 
 
 
-// Get all the fade-in elements
-const fadeInElements = document.querySelectorAll('.fade-in');
+    // Get all the fade-in elements
+    const fadeInElements = document.querySelectorAll('.fade-in');
 
-// Create an intersection observer
-const observer = new IntersectionObserver(function(entries) {
-  // Loop through the entries
-  entries.forEach(function(entry) {
-    // If the element is intersecting
-    if (entry.isIntersecting) {
-      // Use GSAP to animate the element
-      gsap.from(container.querySelectorAll("*"), 
-      { opacity: 0, duration: 1.6, ease: "power1.out", stagger: 0.2 });
+    // Create an intersection observer
+    const observer = new IntersectionObserver(function(entries) {
+    // Loop through the entries
+    entries.forEach(function(entry) {
+        // If the element is intersecting
+        if (entry.isIntersecting) {
+        // Use GSAP to animate the element
+        gsap.from(container.querySelectorAll("*"), {
+            opacity: 0, y: 50,
+            duration: 1.6,
+            ease: "power1.out",
+            stagger: 0.2, delay: 0.2
+        });
 
-      // Stop observing the element
-      observer.unobserve(entry.target);
-    }
-  });
-});
+        // Stop observing the element
+        observer.unobserve(entry.target);
+        }
+    });
+    });
 
-// Observe all the fade-in elements
-fadeInElements.forEach(function(element) {
-  observer.observe(element);
-});
+    // Observe all the fade-in elements
+    fadeInElements.forEach(function(element) {
+    observer.observe(element);
+    });
 
 
 
