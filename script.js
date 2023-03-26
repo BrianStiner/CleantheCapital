79a6b3d90a9d85cf49fa9e7ae61ee9d82785d1e7
Brian Stiner <BrianStinerProfessional@gmail.com>
2023-03-26 13:05:42 -0700

    Now that js in head it needs to wait till loaded

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-// Set scroll threshold
-const scrollThreshold = 400;
+window.addEventListener('load', function() {
 
-// Get a reference to #logo
-const logoElement = document.getElementById('logo');
-// Get the root element
-var r = document.querySelector(':root');
+    // Set scroll threshold
+    const scrollThreshold = 400;
 
-// Used to modify elements to fit nicely on the page.
-calcleft = (window.innerWidth - 1268) / 2 + 20;
-r.style.setProperty('--left', calcleft + 'px');
+    // Get a reference to #logo
+    const logoElement = document.getElementById('logo');
+    // Get the root element
+    var r = document.querySelector(':root');
+
+    // Used to modify elements to fit nicely on the page.
+    calcleft = (window.innerWidth - 1268) / 2 + 20;
+    r.style.setProperty('--left', calcleft + 'px');
 
 
-console.log(logoElement.classList.contains('big'));
-// Listen for scroll events on window, if far enough down shrink logo, and reverse.
-window.addEventListener('scroll', () => {
-    if (logoElement.classList.contains('big')) {
-        if (window.scrollY > scrollThreshold) {
-            logoElement.classList.remove('big');
+    console.log(logoElement.classList.contains('big'));
+    // Listen for scroll events on window, if far enough down shrink logo, and reverse.
+    window.addEventListener('scroll', () => {
+        if (logoElement.classList.contains('big')) {
+            if (window.scrollY > scrollThreshold) {
+                logoElement.classList.remove('big');
+            }
+        } else {
+            if (window.scrollY < scrollThreshold) {
+                logoElement.classList.add('big');
+            }
         }
-    } else {
-        if (window.scrollY < scrollThreshold) {
-            logoElement.classList.add('big');
-        }
+    });
+
+    if (window.innerWidth < 1268) {
+        logoElement.classList.remove('big');
     }
+
 });
-
-if (window.innerWidth < 1268) {
-    logoElement.classList.remove('big');
-}
-
