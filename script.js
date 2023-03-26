528f3a60660a5996f11d8bb818f8cc8512317822
Brian Stiner <BrianStinerProfessional@gmail.com>
2023-03-26 15:44:29 -0700

    start off invisible

--- script.js
+++ script.js
@@ -35,6 +35,11 @@ window.addEventListener('load', function() {
     // Get all the fade-in elements
     const fadeInElements = document.querySelectorAll('.fade-in');
 
+    // Use GSAP to recursively initialize the elements to be invisible
+    forEach(fadeInElements.querySelectorAll('*'), function(element) {
+        gsap.set(element, { opacity: 0, y: -50 });
+    });
+
     // Create an intersection observer
     const observer = new IntersectionObserver(function(entries) {
     // Loop through the entries
@@ -63,3 +68,7 @@ window.addEventListener('load', function() {
 
 
 });
+
+
+
+// 1pvkrpIkDjUfBiQTf8rq5psSG58aQ2fhTqrG2ojIrLY_FXQ9w2BQcC2hSTEFmJvz_0Dz73s9qZkKfQgDYzPw9TwYqRXPJcNyhstqN7D6wD3f7zmrP1wZaEgvj8Quwji5lvCA7M1oCsC1wE_Q_nJW-GTq8_Xpnwd0Ixrbiqa2BEDa2p63-FUARPoNaNvYboBFaPeBxNVeivzAAfIMpvsOirXyHQ9j2oYxXgHxQF9-_ghk
